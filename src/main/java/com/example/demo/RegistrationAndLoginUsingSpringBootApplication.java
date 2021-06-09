9e618b2e43e8d7459c02a9c40357e8b0576b7c26
Training <sablesaurabh98@gmail.com>
2021-06-09 06:43:18 +0000

    First-Commit

---
+++ src/main/java/com/example/demo/RegistrationAndLoginUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLoginUsingSpringBootApplication.class, args);
+	}
+
+}
